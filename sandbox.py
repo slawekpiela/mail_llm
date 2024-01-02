799a0ecf5b0c4003e2433e87838cdddf5eda6d2d
slawek <slawek.piela@koios-mail.pl>
2024-01-02 17:25:32 +0100

    added streamlit testing in sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,24 @@
 import streamlit as st
 
 def main():exit()
-st.title("Simple Streamlit App")
+options_list = ["Inspekcje", "Incydenty", "Mobilne", "Osoby"]
+
+st.title("Zbieranie danych do KOIOS")
 
 # Create two text input boxes
-input1 = st.text_input("Enter first value")
-input2 = st.text_input("Enter second value")
+input1 = st.text_input("Pytanie:")
+input2 = st.text_input("Odpowiedź")
+selected_option = st.selectbox("Choose an option", options_list)
+
+data_to_save=f'{input1},{input2},{st.selectbox}'
+print(data_to_save)
 
 # Create a button that when clicked, shows a message with the input values
-if st.button("Show Inputs"):
-        st.success(f"You entered: {input1} and {input2}")
+if st.button('Zapisz'):
+    with open('output.txt', 'w') as file:
+        file.write(data_to_save)
+    st.success('Data successfully saved to "output.txt"')
+print(data_to_save)
 
 if __name__ == "__main__":
     main()
