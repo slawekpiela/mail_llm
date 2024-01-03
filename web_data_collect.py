19d1ed547ad287b46b2bacdb08428c21001ee87e
slawek <slawek.piela@koios-mail.pl>
2024-01-03 13:20:59 +0100

    data collection minor changes2

--- web_data_collect.py
+++ web_data_collect.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():exit()
 options_list = ["Inspekcje", "Incydenty", "Mobilne", "Osoby"] # clases of answers
-st.title("Zbieranie danych do KOIOS v.1.1")
+st.title("Zbieranie danych do KOIOS v.1.2")
 if 'input1' not in st.session_state:
         st.session_state.input1=''
 
@@ -12,13 +12,13 @@ input1 = st.text_input("Pytanie:")
 input2 = st.text_input("Odpowiedź")
 selected_option = st.selectbox("Choose an option", options_list)
 
-data_to_save=f'{input1},{input2},{selected_option}'
+data_to_save=f'{input1},{input2},{selected_option},\n'
 print(data_to_save)
 
 # Create a button that when clicked, shows a message with the input values
 if st.button('Zapisz'):
     if input1 and input2:
-        with open('/Users/slawekpiela/PycharmProjects/mail_llm/output.txt', 'a') as file:
+        with open('/mount/src/mail_llm/output.txt', 'a') as file:
             file.write(data_to_save)
 
             print(data_to_save)
