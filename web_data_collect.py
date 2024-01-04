cb52e1d7478ad18d3ef8a8afaddb04d45320ce56
slawek <slawek.piela@koios-mail.pl>
2024-01-04 12:27:36 +0100

    aesthetic fixes

--- web_data_collect.py
+++ web_data_collect.py
@@ -16,18 +16,21 @@ if response.status_code == 200:
 else:
     print("nok")
 
-def main():exit()
-options_list = ["Inspekcje", "Incydenty", "Mobilne", "Osoby"] # clases of answers
+
+def main(): exit()
+
+
+options_list = ["Inspekcje", "Incydenty", "Mobilne", "Osoby"]  # clases of answers
 st.title("Zbieranie danych do KOIOS v.1.2")
 if 'input1' not in st.session_state:
-        st.session_state.input1=''
+    st.session_state.input1 = ''
 
 # Create two text input boxes
 input1 = st.text_input("Pytanie:")
 input2 = st.text_input("Odpowiedź")
 selected_option = st.selectbox("Choose an option", options_list)
 
-data_to_save=f'{input1},{input2},{selected_option},\n'
+data_to_save = f'{input1},{input2},{selected_option},\n'
 print(data_to_save)
 
 # Create a button that when clicked, shows a message with the input values
