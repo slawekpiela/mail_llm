055f6468a02218fbf7b6d5ee14c0bde38f84bbad
slawek <slawek.piela@koios-mail.pl>
2024-01-09 19:12:48 +0100

    added session handling

--- web_data_collect.py
+++ web_data_collect.py
@@ -11,6 +11,13 @@ env_airtable_token = os.getenv('AIRTABLE_TOKEN')
 env_base_id = os.getenv('BASE_ID')
 env_table_id = os.getenv('TABLE_ID')
 
+# Initialize session state variables for input1 and input2
+if 'input1' not in st.session_state:
+    st.session_state['input1'] = ""
+if 'input2' not in st.session_state:
+    st.session_state['input2'] = ""
+
+
 # auth url and headers
 url2 = f"https://api.airtable.com/v0/{env_base_id}/{env_table_id}"
 headers = {
@@ -21,14 +28,15 @@ headers = {
 options_list = ["Inspekcje", "Incydenty", "Aplikacja", "Osoby","Powiadomienia", "Raporty", "Konfiguracja", "Raporty", "Systemowe", "Handlowe", "Funkcjonalne"]
 options_list2 = ["Adam", "Asia", "Ewa", "Kira", "Maria", "Sławek"]
 st.header("Zbieranie danych dla systemu KOIOS")
-st.subheader("ver.1.8")
+st.subheader("ver.1.9")
 
 response = requests.get(url2, headers=headers)  # authenticate in airtable
 
-input1 = st.text_input("Pytanie:")
-input2 = st.text_area("Odpowiedź")
+input1 = st.text_input("Pytanie:", value=st.session_state['input1'])
+input2 = st.text_area("Odpowiedź", value=st.session_state['input2'])
 selected_option = st.selectbox("Wybierz sekcję", options_list)
 selected_option2 = st.selectbox("Osoba uzupełniająca dane", options_list2)
+
 
 data_to_save = f"{input1},{input2},{selected_option},{selected_option},\n"
 print(data_to_save)
@@ -69,9 +77,12 @@ if st.button("Zapisz"):
     if input1 and input2:
         response = requests.post(url2, headers=headers, data=json.dumps(data))  # push to airtable
         if response.status_code == 200:
-            pass
+            # Clear the inputs after successful submission
+            st.session_state['input1'] = ""
+            st.session_state['input2'] = ""
+            st.success("Dane zapisane pomyślnie")
         else:
-            print("Airtable post error")
+            st.error("Airtable post error")
     else:
         st.warning("Wypełnij wszytkie pola")
 
